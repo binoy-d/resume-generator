e50dc630e12595e322e88508da5ac3c65caf2240
Daniel Binoy <dbinoy15@gmail.com>
2020-12-14 16:48:50 -0800

    colors!

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from reportlab.pdfgen import canvas
 from reportlab.lib.units import inch
 from reportlab.lib.pagesizes import A4
+
+WIDTH, HEIGHT = A4
+MARGIN = inch/2
 import json
 #https://www.reportlab.com/docs/reportlab-userguide.pdf
 
@@ -17,21 +20,29 @@ def loadInfo(filename:str)->dict:
 adds header with name, links, and contact info
 '''
 def header(c:canvas.Canvas, info:dict):
-    width, height = A4
     c.setFont("Helvetica-Bold", 20)
     #name
-    c.drawCentredString(width/2,height-inch, info["Info"]["name"])
+    c.drawCentredString(WIDTH/2,HEIGHT-inch, info["Info"]["name"])
     
     #smaller font
     c.setFont("Helvetica", 10)
     #linkedin - left
-    c.drawString(inch/2, height-inch/2, info["Info"]["linkedin"])
-
+    c.drawString(MARGIN, HEIGHT-inch/1.5, info["Info"]["linkedin"])
     #github - right
-    c.drawRightString(width-inch/2, height-inch/2, info["Info"]["github"])
-
+    c.drawRightString(WIDTH-MARGIN, HEIGHT-MARGIN, info["Info"]["github"])
+    #contact info - center below name
+    contact = info["Info"]["email"]+" | "+info["Info"]["phone-number"]+" | "+info["Info"]["site"]
+    c.drawCentredString(WIDTH/2,HEIGHT-(inch*1.25),contact)
     #canvas.drawRightString(x, y, text), drawString(x, y, text)
     
+'''
+add skills section, starting from top
+'''
+def skills(c:canvas.Canvas, info:dict, top:float):
+    c.setFont("Helvetica-Bold", 12)
+    #header - left - red - line below
+    c.setFillColorRGB(196/255, 34/255, 51/255)
+    c.drawString(MARGIN, top-inch/4, "Skills")
 
 
 '''
@@ -40,15 +51,9 @@ using info from info dict, assembles and writes pdf
 def create_pdf(filename:str, info:dict)->None:
     
     c = canvas.Canvas(filename, pagesize = A4)
-    c.setFont("Helvetica", 14)
-
-    '''
-    c.setStrokeColorRGB(0.2,0.5,0.3)
-    c.setFillColorRGB(1,0,1)
-    # draw some lines
-    c.line(0,0,0,1.7*inch)
-    '''
     header(c, info)    
+    skills(c, info, HEIGHT-(inch*1.25))
+
     c.showPage()
     c.save()
 
@@ -59,3 +64,11 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+'''
+c.setStrokeColorRGB(0.2,0.5,0.3)
+c.setFillColorRGB(1,0,1)
+# draw some lines
+c.line(0,0,0,1.7*inch)
+'''
